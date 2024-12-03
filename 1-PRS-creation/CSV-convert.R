48e3c4708a6f3564432837abf9327bc457be5072
ryanmtt <149690217+ryanmtt@users.noreply.github.com>
2024-12-03 11:54:38 +0000

    Update CSV-convert.R

--- 1-PRS-creation/CSV-convert.R
+++ 1-PRS-creation/CSV-convert.R
@@ -1,13 +1,13 @@
 # get csv files in appropriate txt format:
 
 # AD:
-AD_SNPs <- read.csv("CSV-FINAL/AD_SNPs_F2.csv")
+AD_SNPs <- read.csv("CSV-FINAL/AD_MR.csv")
 AD.SNPs.ff <- AD_SNPs[, c("rsid", "effect_allele","Beta")]
 write.table(AD.SNPs.ff, file = "PRS/Effect_AD_Final.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 # SBP:
 # For SBP, need to convert negative effects to the positive effect allele:
-SBP.SNPs <- read.csv("CSV-FINAL/SBP SNPs.csv")
+SBP.SNPs <- read.csv("CSV-FINAL/SBP_MR.csv")
 swap_pos <- function(effect_allele, noneffect_allele, Beta) {
   if (Beta < 0) {
     list(effect_allele= noneffect_allele, noneffect_allele = effect_allele, Beta =  -Beta)
@@ -33,7 +33,7 @@ write.table(SBP.SNPs.t, file = "C:/Users/ryanm/Documents/University/Medical Stat
 DBP.SNPs <- read.csv("CSV-FINAL/DBP SNPs.csv")
 
 # Apply the function to the data
-DBP.SNPs.f <- DBP.SNPs %>%
+DBP.SNPs.f <- DBP_MR.csv %>%
   rowwise() %>%
   mutate(result = list(swap_pos(effect_allele, noneffect_allele, Beta))) %>%
   mutate(effect_allele = result$effect_allele,
@@ -46,6 +46,6 @@ DBP.SNPs.t <- DBP.SNPs.f[, c("rsid", "effect_allele","Beta")]
 write.table(DBP.SNPs.t, file = "PRS/Effect_DBP_Final.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 # WMH:
-WMH_SNPs <- read.csv("CSV-FINAL/WMH SNPs.csv")
+WMH_SNPs <- read.csv("CSV-FINAL/WMH_MR.csv")
 WMH.SNPs.f <- WMH_SNPs[, c("rsid", "effect_allele","Beta")]
 write.table(WMH.SNPs.f, file = "PRS/Effect_WMH_Final.txt", sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
